a2ecae10b6d1647f06bd9fed44cebaa0f72dbe96
Ginjupalli <S545138@nwmissouri.edu>
2021-11-03 22:39:15 -0500

    Added

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 I am intersted in eating.
 
 hosted at <https://github.com/nsginjupalli/fwb38ginjupalli.git>
+class - Gas class Gas{ constructor(type, color, price){this.type = type; this.color = color; this.price = price: } }
--- routes/addmods.js
+++ routes/addmods.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 console.log(`rows ${query.rows}`) 
 console.log(`cols ${query.cols}`)
 
-    res.render('addmods?', { title: `Addmods`,query: query });
+    res.render('addmods', { title: `Addmods`,query: query });
 });
 
 module.exports = router;
