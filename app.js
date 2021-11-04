cea28480ac18f5a4cadb005f5cdbfe5514cecb70
Ginjupalli <S545138@nwmissouri.edu>
2021-11-03 23:24:53 -0500

    added

--- app.js
+++ app.js
@@ -22,10 +22,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/addmods', addmodsRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/doll', dollRouter);
-app.use('/addmods', addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
